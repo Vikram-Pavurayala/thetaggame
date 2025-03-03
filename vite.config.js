1560a7801e568698ba22d8044f06eaeea2025719
Vikram-Pavurayala <127219131+Vikram-Pavurayala@users.noreply.github.com>
2025-03-03 22:34:52 +0530

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    proxy: {
+      '/socket.io': {
+        target: 'http://localhost:3000',
+        ws: true,
+      }
+    }
+  }
+});
